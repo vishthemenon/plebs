06df7803c564a71bbf66a68d7916b7595ea0abc7
DanieleVBella <danielebella2000@gmail.com>
2019-07-16 10:00:17 +0100

    Added validations to subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true, format:
+  {
+    with: /\A[A-Z]{2}\S{5}/,
+    message: 'has invalid format'
+  }
+  validates :year, presence: true, inclusion: 1..4
 end
