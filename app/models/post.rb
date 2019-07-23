754bf6ae622e91674b39d69e1ac92a85c4bd7a24
DanieleVBella <danielebella2000@gmail.com>
2019-07-23 17:48:16 +0100

    Implemented uploading documents for posts
    User can now save a document with his post. Model validates document
    type to make sure it is a pdf document. Link for downloading can be
    found in posts/show.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :subject
 
   has_one_attached :document
+
+  validate :document_mime_type
+
+  private
+
+  def document_mime_type
+    if document.attached? && !document.content_type.in?('application/pdf')
+      errors.add(:document, 'Must be a PDF file')
+    end
+  end
+
 end
