ce547eab33fe8b1187e087944c6449f193ebd475
DanieleVBella <danielebella2000@gmail.com>
2019-07-25 16:17:33 +0100

    Added relation between subjects and users
    In future will allow users to save a list of subjects.
    
    Uses a has_and_belongs_to_many relationship.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_145838) do
+ActiveRecord::Schema.define(version: 2019_07_25_151623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,13 @@ ActiveRecord::Schema.define(version: 2019_07_25_145838) do
     t.string "code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subjects_users", id: false, force: :cascade do |t|
+    t.bigint "subject_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["subject_id", "user_id"], name: "index_subjects_users_on_subject_id_and_user_id"
+    t.index ["user_id", "subject_id"], name: "index_subjects_users_on_user_id_and_subject_id"
   end
 
   create_table "users", force: :cascade do |t|
