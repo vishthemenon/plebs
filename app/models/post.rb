009ed907421f2a0bf912d9032922eaa026be1230
DanieleVBella <danielebella2000@gmail.com>
2019-07-25 15:59:28 +0100

    Added relation between users and posts
    Each user has many users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :subject
+  belongs_to :user
 
   has_one_attached :document
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_124744) do
+ActiveRecord::Schema.define(version: 2019_07_25_145838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,9 @@ ActiveRecord::Schema.define(version: 2019_07_25_124744) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.bigint "user_id", null: false
     t.index ["subject_id"], name: "index_posts_on_subject_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -117,4 +119,5 @@ ActiveRecord::Schema.define(version: 2019_07_25_124744) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "learning_outcomes", "subjects"
   add_foreign_key "posts", "subjects"
+  add_foreign_key "posts", "users"
 end
