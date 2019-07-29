0a850bc68ff1232581b1472fd205c3a826d82419
DanieleVBella <danielebella2000@gmail.com>
2019-07-29 16:14:58 +0100

    Added and implemented posts controller
    User can now like and unlike posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :lecturers
   resources :subjects
   resources :posts, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :likes, only: [:create, :destroy]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
