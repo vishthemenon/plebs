5235198d3224b8659014d5894e064da9ac20061f
DanieleVBella <danielebella2000@gmail.com>
2019-07-30 12:29:07 +0100

    Added link to toggle favourite status on subjects
    Added to subjects index page.
    
    Two new controller methods, with appropriate routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :lecturers
   resources :subjects
+  post 'subjects/:id/add_user', to: 'subjects#add_user', as: 'add_user_to_subject'
+  post 'subjects/:id/del_user', to: 'subjects#del_user', as: 'del_user_from_subject'
   resources :posts, only: [:show, :new, :create, :edit, :update, :destroy]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
